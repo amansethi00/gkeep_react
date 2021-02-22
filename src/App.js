dde40c2cfd39c8bf11ae41f9b3f18f866c47299d
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2021-02-22 18:25:53 +0530

    add input and view todo

--- src/App.js
+++ src/App.js
@@ -1,36 +1,96 @@
 import "./App.css";
+import "./App.scss";
 import {useState} from "react";
+import {v4 as uuidv4} from "uuid";
 
+const todo = [
+  {
+    id: uuidv4(),
+    title: "Title",
+    description: "Description",
+    complete: false,
+    pin: false,
+    label: "",
+    bgColor: "",
+  },
+];
 function App() {
   const [input, showInput] = useState(false);
-  function inputHandler() {}
+  const [userinput, setUserinput] = useState("");
+  const [todos, setTodos] = useState(todo);
+
+  const inputTitleHandler = (event) => {
+    setUserinput({...userinput, title: event.target.value, id: uuidv4()});
+  };
+  const inputDescriptionHandler = (event) => {
+    setUserinput({...userinput, description: event.target.value});
+  };
+  const addButtonHandler = () => {
+    if (userinput !== "") {
+      setTodos([...todos, userinput]);
+      setUserinput("");
+      showInput(!input);
+    }
+  };
+  const closeButtonHandler = () => {
+    showInput(!input);
+    setUserinput("");
+  };
+  console.log(userinput);
+  console.log(todos);
   return (
     <div className="App">
-      <h1>Google Keep</h1>
-
-      {input === false ? (
-        <div className="click" onClick={() => showInput(true)}>
-          add note
+      <div className="gkeep">
+        <div className="sidebar">
+          <h1>Tags</h1>
         </div>
-      ) : (
-        <div>
-          {" "}
-          <input
-            onClick={inputHandler}
-            placeholder="add title"
-            className="input-title"
-          ></input>
-          <div>
-            <textarea
-              placeholder="add description"
-              className="input-description"
-            ></textarea>
-          </div>
-          <button onClick={() => showInput(false)}>Close</button>
-        </div>
-      )}
+        <div className="keep-container">
+          {/* <h1>Google Keep</h1> */}
 
-      {/* <div><input></input></div> */}
+          {input === false ? (
+            <div className="addNote" onClick={() => showInput(true)}>
+              add note
+            </div>
+          ) : (
+            <div className="input-div">
+              <input
+                onChange={inputTitleHandler}
+                placeholder="Title"
+                className="input-title"
+              ></input>
+              <div>
+                <textarea
+                  placeholder="add a note..."
+                  className="input-description"
+                  onChange={inputDescriptionHandler}
+                ></textarea>
+              </div>
+              <button onClick={addButtonHandler}>Add</button>
+              <button onClick={closeButtonHandler}>Close</button>
+            </div>
+          )}
+          <ul>
+            <div className="todo-view">
+              {" "}
+              {todos.map((prev) => {
+                return (
+                  <li key={prev.id}>
+                    <div className="todo">
+                      <div className="todo-title">{prev.title}</div>
+                      <div className="todo-description">{prev.description}</div>
+                    </div>
+                  </li>
+                );
+              })}
+            </div>
+          </ul>
+        </div>
+      </div>
+
+      {/* <div className="todo">
+        <div className="todo-title"> Title</div>
+        <div className="todo-description">Description</div>
+      </div> */}
     </div>
   );
 }
